ec6c4fa246a15c3fb6f5c14fe1d91b9a506bfac3
hts7117 <huynhthanhsang7117@gmail.com>
2022-12-20 21:25:27 +0700

    Change Stremlit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pickle
 
+navieBayes = pickle.load(open("NavieBayes.pkl", "rb"))
 
+def predict_from_raw(model, text):
+    return "Đây là tin thật" if model.predict([text]) == 0.0 else "Đây là tin giả"
 
 def main():
     st.title("Fake news detection")
@@ -13,9 +16,13 @@ def main():
     st.write('Options: ', options)
 
     with st.form(key="text"):
-        raw_domain = st.text_area("Domain")
         raw_text = st.text_area("News")
         submit = st.form_submit_button(label='Submit')
+
+    if submit:
+        if options == 'Navie Bayes':
+            model = navieBayes
+            st.write(predict_from_raw(model, raw_text))
 
 if __name__ == '__main__':
     main()
